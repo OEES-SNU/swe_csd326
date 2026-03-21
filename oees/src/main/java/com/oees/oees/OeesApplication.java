bbb6c23ea304498d8a2bfdbbef6a7404e30e4e70
CipherEnigma <nooyi1307@gmail.com>
2026-03-22 04:04:42 +0530

    initial project setup with Spring Boot, Gradle, PostgreSQL and folder structure

---
+++ oees/src/main/java/com/oees/oees/OeesApplication.java
@@ -0,0 +1,13 @@
+package com.oees.oees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OeesApplication.class, args);
+    }
+
+}
---
+++ oees/src/test/java/com/oees/oees/OeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oees.oees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
